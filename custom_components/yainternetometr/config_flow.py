65d63000e21e0e605e2b89474ddd16449f09ef55
Ерилов Никита <nerilov@octopeople.net>
2025-11-14 16:39:00 +0300

    Add comments on config_flow.py

--- custom_components/yainternetometr/config_flow.py
+++ custom_components/yainternetometr/config_flow.py
@@ -3,6 +3,18 @@ from homeassistant import config_entries
 from .const import DOMAIN
 
 class YaInternetometrConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """
+    Config Flow for YaInternetometr integration.
+
+    This class allows the user to add integrations through the Home Assistant UI without having to edit configuration.yaml.
+
+    Inherits:
+        `config_entries.ConfigFlow`: base class for creating interactive configuration flows in Home Assistant.
+
+    Attributes:
+        `VERSION` (int): Configuration structure version. Allows you to manage migration of configurations when changing the integration.
+    """
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
