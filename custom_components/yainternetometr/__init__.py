af9039a2dceae8dde5c578b8eb63cf11d74f1b14
Ерилов Никита <nerilov@octopeople.net>
2025-12-23 12:10:13 +0300

    Fixes

--- custom_components/yainternetometr/__init__.py
+++ custom_components/yainternetometr/__init__.py
@@ -46,10 +46,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = {"coordinator": coordinator}
 
-    await hass.config_entries.async_forward_entry_setups(
-        entry, 
-        ["sensor", "number"]
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "number"])
     return True
 
 
