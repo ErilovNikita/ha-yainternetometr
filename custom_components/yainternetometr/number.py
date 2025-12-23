943e74a63fc00643d399b5c6e5b1bd9b52d62fbd
Ерилов Никита <nerilov@octopeople.net>
2025-12-23 12:42:45 +0300

    Refactoring

--- custom_components/yainternetometr/number.py
+++ custom_components/yainternetometr/number.py
@@ -1,36 +1,45 @@
-from __future__ import annotations
-
+from datetime import timedelta
 from homeassistant.components.number import NumberEntity
 from homeassistant.const import UnitOfTime
-from datetime import timedelta
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 
-from .const import DOMAIN, CONF_UPDATE_INTERVAL, MIN_SCAN_INTERVAL, MAX_SCAN_INTERVAL, STEP_SCAN_INTERVAL
+from .const import DOMAIN, CONF_UPDATE_INTERVAL, DEFAULT_SCAN_INTERVAL, MAX_SCAN_INTERVAL, MIN_SCAN_INTERVAL, STEP_SCAN_INTERVAL
 
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([UpdateIntervalNumber(coordinator)])
 
+    async_add_entities([
+        UpdateIntervalNumber(hass, entry, coordinator)
+    ])
 
 class UpdateIntervalNumber(NumberEntity):
     _attr_name = "Update interval"
-    _attr_unique_id = f"{DOMAIN}_{CONF_UPDATE_INTERVAL}"
-    _attr_unit_of_measurement = UnitOfTime.SECONDS
-    _attr_min_value = MIN_SCAN_INTERVAL
-    _attr_max_value = MAX_SCAN_INTERVAL
-    _attr_step = STEP_SCAN_INTERVAL
-    _attr_mode = "box"
-    _attr_icon = "mdi:timer-cog"
+    _attr_native_unit_of_measurement = UnitOfTime.SECONDS
+    _attr_native_min_value = MIN_SCAN_INTERVAL
+    _attr_native_max_value = MAX_SCAN_INTERVAL
+    _attr_native_step = STEP_SCAN_INTERVAL
 
-    def __init__(self, coordinator):
+    def __init__(self, hass: HomeAssistant, entry: ConfigEntry, coordinator):
+        self.hass = hass
+        self.entry = entry
         self.coordinator = coordinator
-        self._attr_native_value = int(
-            coordinator.update_interval.total_seconds()
+
+        self._attr_unique_id = f"{entry.entry_id}_update_interval"
+
+        self._attr_native_value = entry.options.get(
+            CONF_UPDATE_INTERVAL,
+            DEFAULT_SCAN_INTERVAL
         )
 
     async def async_set_native_value(self, value: float) -> None:
-        seconds = int(value)
-        self._attr_native_value = seconds
+        value = int(value)
 
-        self.coordinator.update_interval = timedelta(seconds=seconds)
+        self._attr_native_value = value
 
-        await self.coordinator.async_request_refresh()
+        self.hass.config_entries.async_update_entry(
+            self.entry,
+            options={**self.entry.options, CONF_UPDATE_INTERVAL: value},
+        )
+
+        self.coordinator.update_interval = timedelta(seconds=value)
