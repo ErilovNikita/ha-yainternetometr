17026560ce22eb9830fbcc8d97b433854cd5e5a9
Ерилов Никита <nerilov@octopeople.net>
2025-12-23 11:54:49 +0300

    Update version

--- custom_components/yainternetometr/__init__.py
+++ custom_components/yainternetometr/__init__.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 import asyncio
 import logging
-from datetime import timedelta
 
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
